a309d26cd49cab9e231b2f5f54695868f6823aa8
revanth <revanth2009@gmail.com>
2018-07-12 22:55:36 -0500

    layout and image load plugin

--- public/js/main.js
+++ public/js/main.js
@@ -1,66 +1,32 @@
 $(document).ready(function () {
     // init Masonry
-    var $grid = $('.grid').masonry({
+    // var $grid = $('.grid').masonry({
+    //     itemSelector: '.grid-item',
+    //     columnWidth: '.grid-sizer',
+    //     percentPosition: true
+    // });
+
+    // // layout Masonry after each image loads
+    // $grid.imagesLoaded().progress(function () {
+    //     $grid.masonry('layout');
+    // });
+
+    $('.grid').imagesLoaded(function () {
+        // init Masonry after all images have loaded
+        gridReset();
+    });
+
+    $('#setGrid').on('change', function(){
+        var percentage = $(this).val();
+        $('.grid-sizer,.grid-item').css('width', percentage +'%');
+        gridReset();
+    });
+});
+
+function gridReset(){
+    $('.grid').masonry({
         itemSelector: '.grid-item',
         columnWidth: '.grid-sizer',
         percentPosition: true
     });
-    
-    // layout Masonry after each image loads
-    $grid.imagesLoaded().progress(function () {
-        $grid.masonry('layout');
-    });
-});
-
-// $(document).ready(function () {
-//     $('.lol').submit(function (event) {
-//         event.preventDefault();
-//         var files = document.getElementById('uImg').files;
-//         var filesArr = [];
-
-//         test(files);
-//         // for (var i = 0; i < files.length; i++) {
-//         //     console.log(files[i]);
-//         //     var formData = new FormData(files[i]);
-//         //     console.log(formData);
-//         //     $.ajax({
-//         //         url: '/uploads',
-//         //         data: files[i],
-//         //         type: "POST",
-//         //         // mimeType: "multipart/form-data",
-//         //         contentType: false,
-//         //         cache: false,
-//         //         processData: false,
-
-//         //         success: function (data) {
-//         //             console.log(data);
-//         //         },
-//         //         error: function (data) {
-//         //             console.error(data);
-//         //         }
-//         //     });
-//         // }
-//         // console.log(filesArr);
-//     });
-// });
-
-// function test(file) {
-//     var formData = new FormData(file);
-
-//     $.ajax({
-//         url: '/uploads',
-//         data: formData,
-//         type: "POST",
-//         mimeType: "multipart/form-data",
-//         contentType: false,
-//         cache: false,
-//         processData: false,
-
-//         success: function (data) {
-//             console.log(data);
-//         },
-//         error: function (data) {
-//             console.error(data);
-//         }
-//     });
-// }
+}

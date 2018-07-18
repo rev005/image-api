2faeb8555b8fa17bdfdb155666b0cb4f56b574e4
revanth <revanth2009@gmail.com>
2018-07-17 21:47:43 -0500

    file setup for model service and controller

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var bodyParser = require('body-parser');
 var multer = require('multer');
 
 var index = require('./routes/index');
+var bluebird = require('bluebird');
+var mongoose = require('mongoose');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -60,6 +62,18 @@ app.post('/uploads', multer(multerConfig).array('photo',500), function (req, res
     res.send('failed');
   }
   //res.send('Complete! Check out your public/img folder.  Please note that files not encoded with an image mimetype are rejected. <a href="/">try again</a>');
+});
+
+mongoose.Promise = bluebird
+mongoose.connect('mongodb://127.0.0.1:27017/wpixy', { useMongoClient: true})
+.then(()=> { console.log(`Succesfully Connected to the Mongodb Database  at URL : mongodb://127.0.0.1:27017/todoapp`)})
+.catch(()=> { console.log(`Error Connecting to the Mongodb Database at URL : mongodb://127.0.0.1:27017/todoapp`)})
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
 });
 
 module.exports = app;
